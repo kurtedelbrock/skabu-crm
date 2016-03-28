f3bdb1cc642ae770f22095ae7b5220ee110b215d
Kurt Edelbrock <kurt.edelbrock@careerbuilder.com>
2016-03-28 14:17:47 -0400

    Adding JSON rendering for responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @users = User.all
+    render json: @users
   end
 
   def create
     @user = User.create params.require(:user).permit(:email)
+    render json: @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/create'
+  resources :users, only: [:index, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
